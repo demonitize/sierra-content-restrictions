7230039b5a3f441a50ab1e7883550f44d17c4bfc
DEMONITIZED BOI <42183865+demonitize@users.noreply.github.com>
2021-12-02 16:09:30 -0800

    Add files via upload

--- background.js
+++ background.js
@@ -1,33 +1,33 @@
-var blockedPages;
-var redirectPage;
+  var blockedPages;
+  var redirectPage;
 
 
-chrome.storage.managed.get('RestrictionsEnabled', (data) => {
-  if (data.RestrictionsEnabled) {
-    chrome.action.setPopup({popup: 'popup.html'});
-  } else {
-    chrome.action.setPopup({popup: 'popup_idle.html'});
-  }
-});
+  chrome.storage.managed.get('RestrictionsEnabled', (data) => {
+    if (data.RestrictionsEnabled) {
+      chrome.action.setPopup({popup: 'popup.html'});
+    } else {
+      chrome.action.setPopup({popup: 'popup_idle.html'});
+    }
+  });
 
-chrome.storage.managed.get('BlockedPages', (data) => {
-  blockedPages = data.BlockedPages;
-  }
-);
+  chrome.storage.managed.get('BlockedPages', (data) => {
+    blockedPages = data.BlockedPages;
+    }
+  );
 
-chrome.storage.managed.get('RedirectEnabled', (data) => {
-    if (data.RedirectEnabled) {
-      chrome.storage.managed.get('RedirectTo', (data) => {
-        redirectPage = data.RedirectTo;             
-      });  
-  } else {
-  redirectPage = "/block.html";
-  }
-});
-chrome.webRequest.onBeforeRequest.addListener(
-  function(details) {
-    if (blockedPages.contains(details.url))
-    return {cancel: true, redirectUrl: redirectPage};
-  },
-  ["blocking"]
-);
+  chrome.storage.managed.get('RedirectEnabled', (data) => {
+      if (data.RedirectEnabled) {
+        chrome.storage.managed.get('RedirectTo', (data) => {
+          redirectPage = data.RedirectTo;             
+        });  
+    } else {
+    redirectPage = "/block.html";
+    }
+  });
+  chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+      if (blockedPages.contains(details.url))
+      return {cancel: true, redirectUrl: redirectPage};
+    },
+    ["blocking"]
+  );
